ae2994292b502b98c906be7937440bff28e8a72c
tekito <tekito@kk.kk>
2019-12-16 20:32:10 +0900

    シーン繊維用のクラスを作成

---
+++ 2019Tryout_TeamF_UnityProject/Assets/Shiraeda/Script/SceneCtl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtl : MonoBehaviour
+{
+    // インスタンス
+    public static SceneCtl instance;
+    // シーンマネージャー
+    private SceneManager scene;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            // thisだとオブジェクトが残るのでループすると大量のオブジェクトが発生する
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void GetScene()
+    {
+    }
+
+    public void NextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
