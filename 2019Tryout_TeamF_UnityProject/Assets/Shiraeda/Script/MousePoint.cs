cb61121f7435c20fad1d2c8c6899a10f2520213f
tekito <tekito@kk.kk>
2019-12-16 20:08:20 +0900

    白枝ファイルを追加 マウス判定用スクリプトMousePoint.csを追加 ゴムまく生成用(長さ変換用)スクリプトLineCreate.csを追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/Shiraeda/Script/MousePoint.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePoint : MonoBehaviour
+{
+    // ワールド座標
+    private Vector3 _worldPos;
+    // 2点間の距離
+    private float _distance;
+    // 2点間の角度
+    private float _angle;
+
+    // デバック情報
+    [SerializeField, Header("Debug")]
+    private bool _debug = false;
+    // 表示用オブジェクト
+    [SerializeField, Tooltip("デバック用表示物")]
+    private GameObject _debugObj = null;
+    private GameObject _obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _worldPos = Vector3.zero;
+        _distance = 0f;
+        _angle = 0f;
+
+        if (_debug && _debugObj != null)
+        {
+            _obj = Instantiate(_debugObj);
+        }
+    }
+
+    public Vector3 GetMousePoint()
+    {
+        // マウス座標を取得
+        Vector3 mousePos = Input.mousePosition;
+        // スクリーンのXbox
+        _worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        _worldPos.z = 10.0f;
+        if(_debug && _obj != null)
+        {
+            _obj.transform.position = _worldPos;
+        }
+        return _worldPos;
+    }
+    // 2点間の距離
+    public float GetDistance(Vector2 pos1, Vector2 pos2)
+    {
+        _distance = Vector2.Distance(pos1, pos2);
+        // 仮の戻り値
+        return _distance;
+    }
+    // 2点間の角度の差
+    public float GetAngle(Vector2 pos1, Vector2 pos2)
+    {
+        float vecX = (pos2.x - pos1.x);
+        float vecY = (pos2.y - pos1.y);
+        float rad = Mathf.Atan2(vecY, vecX);
+        _angle = rad * Mathf.Rad2Deg;
+        return _angle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
