33163e2c9c715fbbae01f2884ba4b1d8ea9d699b
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2019-12-16 20:01:20 +0900

    ボールの動きを追加

--- 2019Tryout_TeamF_UnityProject/Assets/OkabeTatuya/Script/BallScript.cs
+++ 2019Tryout_TeamF_UnityProject/Assets/OkabeTatuya/Script/BallScript.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BallScript : MonoBehaviour
 {
+    [SerializeField] float m_speed;
+    [SerializeField] Vector3 m_startMoveVector;
+    [SerializeField] Vector3 m_stratPosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetPosition();
+        StartMove();
+        AudioManager.Instance.PlaySound();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void ResetPosition()
+    {
+        transform.position = m_stratPosition;
+    }
+
+    void StartMove()
+    {
+        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+        Vector3 force = m_startMoveVector * m_speed;
+        rb.AddForce(force);
     }
 }
