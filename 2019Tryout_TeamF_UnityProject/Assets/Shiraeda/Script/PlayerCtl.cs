6faf4b153adf440cffb96d33644b287c2ffce36e
tekito <tekito@kk.kk>
2019-12-17 16:44:36 +0900

    プレイヤーの追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/Shiraeda/Script/PlayerCtl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtl : MonoBehaviour
+{
+    [SerializeField, Tooltip("キャラクターの移動速度")]
+    private float _speed;
+    struct InputButton
+    {
+    }
+    void Start()
+    {
+    }
+
+    private void GetInput()
+    {
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right * _speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ダメージ");
+    }
+}
