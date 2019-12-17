07ff70381a3802f4a8b2c09f58cc512bdd454ed7
Hikka12044021 <11806061@stu.kbc.ac.jp>
2019-12-17 16:23:45 +0900

    エネミーの動き、弾の発射

---
+++ 2019Tryout_TeamF_UnityProject/Assets/TouyamaHikaru/Scripts/Bullet.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //public byte type;
+    public GameObject Shotprefab;
+    public float E_ShotSpeed;
+    protected GameObject clone;
+    protected float EnemyShotTime;
+    protected float endTimeBullet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public GameObject ShotClone()
+    {
+
+
+        EnemyShotTime += Time.deltaTime;
+        
+        if (EnemyShotTime >= 1.0f)
+        {
+            Transform myTransform = this.transform;
+
+            clone = Instantiate(Shotprefab, myTransform.position, Quaternion.identity);
+
+
+            EnemyShotTime = 0;
+
+            return clone;
+        }
+
+        return null;
+
+    }
+
+
+
+    public void BulletDestroy(GameObject Bullet)
+    {
+        endTimeBullet += Time.deltaTime;
+
+        if (!(Bullet.transform.position.x > 0 && Bullet.transform.position.x < Screen.width)
+            ||!(Bullet.transform.position.y > 0 && Bullet.transform.position.y < Screen.height))
+        {
+            Destroy(Bullet);
+     
+        }
+    }
+
+
+}
---
+++ 2019Tryout_TeamF_UnityProject/Assets/TouyamaHikaru/Scripts/Suraimu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suraimu : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        enemyX = myTransform.position.x;
+        enemyY = myTransform.position.y;
+
+        direction = true;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        LateralMovement();
+
+    }
+}
