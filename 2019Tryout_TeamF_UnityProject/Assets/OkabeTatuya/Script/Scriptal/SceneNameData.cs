83faf7102fe5815b82a9ac257120720e1df37270
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2019-12-17 16:10:38 +0900

    ScriptableObjectの追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/OkabeTatuya/Script/Scriptal/SceneNameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CreateScriptable/SceneNameData")]
+public class SceneNameData : ScriptableObject
+{
+    public List<string> sceneName;
+}
