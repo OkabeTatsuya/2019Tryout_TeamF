9cc789ca19983872fc35c31d25650f5009acdaa7
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2019-12-18 11:55:33 +0900

    スクリプトの追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/OkabeTatuya/Script/Scriptal/GameManagerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CreateScriptable/Manager/GameManagerData")]
+public class GameManagerData : ScriptableObject
+{
+    public float m_nowScore { get; set; }
+    
+
+}
