d38f9300702052880bae396f366e301ae5d5c99c
Hikka12044021 <11806061@stu.kbc.ac.jp>
2019-12-18 15:47:08 +0900

    dragonのスクリプト変更、壁に当たるようになりました。

--- 2019Tryout_TeamF_UnityProject/Assets/TouyamaHikaru/Scripts/Enemy.cs
+++ 2019Tryout_TeamF_UnityProject/Assets/TouyamaHikaru/Scripts/Enemy.cs
@@ -13,7 +13,8 @@ public class Enemy : MonoBehaviour
     protected float enemyY;
     protected bool direction;  //X軸　false:左  true:右    //Y軸　false:上　true:下
     protected bool directionSwitch; //方向を変えるための変数   false:上下    true:左右
-    protected float directioncount;  //方向を変えるたびにカウントが増える 
+    protected float directionCount;  //方向を変えるたびにカウントが増える （コウモリ用）
+    protected float directionCount2; //方向を変えるたびにカウントが増える （ドラゴン用）
 
     public int hp;
     public int hp_max;
@@ -36,7 +37,7 @@ public class Enemy : MonoBehaviour
         // transformを取得
         Transform myTransform = this.transform;
 
-        directioncount += Time.deltaTime;
+        
 
         if (direction == true)
         {
@@ -95,16 +96,16 @@ public class Enemy : MonoBehaviour
 
         if(direction == true)
         {
-            directioncount += Time.deltaTime;
+            directionCount += Time.deltaTime;
         }
         else
         {
-            directioncount -= Time.deltaTime;
+            directionCount -= Time.deltaTime;
         }
 
-        Debug.Log((int)directioncount/1 % 4);
+        Debug.Log((int)directionCount / 1 % 4);
 
-        switch((int)directioncount/1 % 4)
+        switch((int)directionCount / 1 % 4)
         {
             case 0:
                 myTransform.Translate(speedX, -speedY, 0);
@@ -122,45 +123,6 @@ public class Enemy : MonoBehaviour
         }
 
 
-        //myTransform.Translate(speedX, -speedY, 0);
-
-
-        //if (direction == true)
-        //{
-        //    //座標移動
-        //    myTransform.Translate(speedX, -speedY, 0);
-
-        //    if (enemyX + moveX <= myTransform.position.x)
-        //    {
-        //        myTransform.position = new Vector3(enemyX + moveX, myTransform.position.y, 0);
-        //        speedX *= -1;
-        //    }
-
-        //    if(enemyY - moveY*2 >= myTransform.position.y)
-        //    {
-        //        speedY *= -1;
-        //        myTransform.position = new Vector3(myTransform.position.x, enemyY - moveY * 2, 0);
-        //        direction = false;
-        //    }
-
-        //}
-
-        //else if (direction == false)
-        //{
-        //    myTransform.Translate(speedX, -speedY, 0);
-
-        //    if (enemyX - moveX >= myTransform.position.x)
-        //    {
-        //        myTransform.position = new Vector3(enemyX - moveX, myTransform.position.y, 0);
-        //        speedX *= -1;
-        //    }
-        //    if (enemyY <= myTransform.position.y)
-        //    {
-        //        speedY *= -1;
-        //        myTransform.position = new Vector3(myTransform.position.x, enemyY, 0);
-        //        direction = true;
-        //    }
-        //}
 
     }
 
@@ -169,7 +131,7 @@ public class Enemy : MonoBehaviour
         // transformを取得
         Transform myTransform = this.transform;
 
-        if (directionSwitch == true && direction == true && directioncount != 4)  //横移動
+        if (directionSwitch == true && direction == true && directionCount2 != 4)  //横移動
         {
             // 座標移動
             myTransform.Translate(speedX, 0, 0);
@@ -177,24 +139,24 @@ public class Enemy : MonoBehaviour
             if (enemyX + moveX <= myTransform.position.x)
             {
                 direction = false;
-                directioncount++;
+                directionCount2++;
             }
         }
 
-        else if (directionSwitch == true && direction == false && directioncount != 4)
+        else if (directionSwitch == true && direction == false && directionCount2 != 4)
         {
             myTransform.Translate(-speedX, 0, 0);
 
             if (enemyX - moveX >= myTransform.position.x)
             {
                 direction = true;
-                directioncount++;
+                directionCount2++;
             }
         }
 
-        
 
-        if (directionSwitch == false && direction == true && directioncount != 4)  //縦移動
+
+        if (directionSwitch == false && direction == true && directionCount2 != 4)  //縦移動
         {
             //座標移動
             myTransform.Translate(0, speedY, 0);
@@ -202,35 +164,35 @@ public class Enemy : MonoBehaviour
             if (enemyY + moveY <= myTransform.position.y)
             {
                 direction = false;
-                directioncount++;
+                directionCount2++;
             }
         }
 
-        else if (directionSwitch == false && direction == false&& directioncount != 4)
+        else if (directionSwitch == false && direction == false && directionCount2 != 4)
         {
             myTransform.Translate(0, -speedY, 0);
 
             if (enemyY - moveY >= myTransform.position.y)
             {
                 direction = true;
-                directioncount++;
+                directionCount2++;
             }
         }
 
-        if (directioncount == 4)
+        if (directionCount2 == 4)
         {
 
 
-            if(directionSwitch == true)
+            if (directionSwitch == true)
             {
                 // 座標移動
                 myTransform.Translate(speedX, 0, 0);
 
                 if (enemyX <= myTransform.position.x)
                 {
-                    
+
                     directionSwitch = false;
-                    directioncount = 0;
+                    directionCount2 = 0;
                 }
             }
             else
@@ -241,7 +203,7 @@ public class Enemy : MonoBehaviour
                 if (enemyY <= myTransform.position.y)
                 {
                     directionSwitch = true;
-                    directioncount = 0;
+                    directionCount2 = 0;
                 }
 
             }
@@ -276,9 +238,10 @@ public class Enemy : MonoBehaviour
         {
             Debug.Log("a");
 
-            directioncount += 2;
+            directionCount += 2;
+            directionCount2++;
 
-            if(direction == true)
+            if (direction == true)
             {
                 direction = false;
             }
