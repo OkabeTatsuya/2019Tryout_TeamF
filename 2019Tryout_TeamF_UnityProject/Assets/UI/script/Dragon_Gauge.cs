f1d4b3540363cfed89644b9547ecc9ffa123c44e
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2019-12-18 09:49:44 +0900

    UIの追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/UI/script/Dragon_Gauge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dragon_Gauge : MonoBehaviour
+{
+    [SerializeField]
+    Image HPGauge; // HP ゲージ
+    [SerializeField]
+    private GameObject dragon;
+    Dragon script;//Dragonスクリプトの参照
+
+    void Start()
+    {
+        script = dragon.GetComponent<Dragon>();//ドラゴンの情報の取得
+    }
+
+    void Update()
+    {
+        var Min = script.hp;
+        var Max = script.hp_max;
+        HPGauge.fillAmount = (float)Min / Max;
+
+    }
+}
---
+++ 2019Tryout_TeamF_UnityProject/Assets/UI/script/Slime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    public int hp = 5;
+    public int hp_max = 5;
+
+    void Update()
+    {
+        var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        var dv = 6f * Time.deltaTime * direction;
+        transform.Translate(dv.x, dv.y, 0.0f);
+        if (hp <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    public void click()
+    {
+        hp--;
+    }
+}
