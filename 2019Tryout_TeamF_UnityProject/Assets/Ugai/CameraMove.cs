3163f61e2984bd3bb10980f0dcc767c258eaffca
nkcUgai <iubu0503@gmail.com>
2019-12-18 19:42:48 +0900

    ウェーブ切り替え処理開発中

---
+++ 2019Tryout_TeamF_UnityProject/Assets/Ugai/CameraMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform player;
+    float nowtime = 0f;
+    float totaltime = 3f;
+
+    float speed = 25;
+    public GameObject[] WavePoint;
+    bool WaveMove = false;
+    void Start()
+    {
+        
+    }
+    float y = 0;
+    Vector3 startPos;
+    void Update()
+    {
+        //nowtime += Time.deltaTime;
+        //transform.position = Vector3.Lerp(this.transform.position, new Vector3(transform.position.x, y, -10), nowtime / totaltime);
+        
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            WaveMove = true;
+            startPos = transform.position;
+        }
+        if (WaveMove)
+        {
+            //transform.position = Vector3.Lerp(startPos, WavePoint[0], this.transform.position)
+            return;
+        }
+
+        if (player != null)
+        {
+            this.transform.position = new Vector3(transform.position.x, player.position.y, -10);
+            Clamp();
+        }
+    }
+
+    private Vector3 camera_pos;
+    void Clamp()
+    {
+        camera_pos = transform.position; //プレイヤーの位置を取得
+
+        camera_pos.y = Mathf.Clamp(camera_pos.y, 0, 48.0f); //x位置が常に範囲内か監視
+        transform.position = new Vector3(camera_pos.x, camera_pos.y, -10); //範囲内であれば常にその位置がそのまま入る
+    }
+}
--- 2019Tryout_TeamF_UnityProject/Assets/Ugai/EnemyPop.cs
+++ 2019Tryout_TeamF_UnityProject/Assets/Ugai/EnemyPop.cs
@@ -2,29 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
-public class EnemyPop : MonoBehaviour
+public class EnemyPop : SingletonMonoBehaviour<EnemyPop>
 {
     public SatgeDatabase Stage_Database;
-    public GameObject te;
-    void Awake()
-    {
-        Export_MapData Data;
-        Data = JsonConvert.DeserializeObject<Export_MapData>(Stage_Database.Stage.Wave[0].ToString());
+    public GameObject[] EnemyPoint;
+    int nowWave = 0, enemyCou = 0;
 
-        for (int i = 0; i < Data.MapData.Count; i++)
-        {
-            GameObject Enemy = Instantiate(Stage_Database.EnemyList[Data.MapData[i].EnemyNo]);
-            Enemy.transform.parent = this.transform;
-            Enemy.transform.localPosition = new Vector3(Data.MapData[i].X, -Data.MapData[i].Y, 0);
-            Enemy.transform.localScale = new Vector3(Data.MapData[i].size, Data.MapData[i].size, 1);
-        }
-    }
     void Start()
     {
-        
+        Enemy_Set();
     }
     void Update()
     {
+
+    }
+
+
+
+    void Enemy_Set()
+    {
+        Import_MapData Data;
+        Data = JsonConvert.DeserializeObject<Import_MapData>(Stage_Database.Stage.Wave[nowWave].ToString());
+        for (int i = 0; i < Data.MapData.Count; i++)
+        {
+            GameObject Enemy = Instantiate(Stage_Database.EnemyList[Data.MapData[i].EnemyNo]);
+            Enemy.transform.parent = EnemyPoint[nowWave].transform;
+            Enemy.transform.localPosition = new Vector3(Data.MapData[i].X, -Data.MapData[i].Y, 0);
+            Enemy.transform.localScale = new Vector3(Data.MapData[i].size, Data.MapData[i].size, 1);
+        }
         
+    }
+
+    public class Import_MapData
+    {
+        public List<EnemyData> MapData;
     }
 }
