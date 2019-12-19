72f671a9636d869e13259fa1fa7df9236e6ccb5e
rpg12345 <shinsekainokamida@yahoo.co.jp>
2019-12-19 18:52:13 +0900

    ファイルの追加

---
+++ 2019Tryout_TeamF_UnityProject/Assets/OsakiRyoto/Enemy_remake.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_remake : MonoBehaviour
+{
+    
+    public float moveX;     //どこまで動くか
+    public float moveY;
+    public float speedX;    //スピード
+    public float speedY;
+    protected float enemyX;      //置いた座標
+    protected float enemyY;
+    protected bool direction;  //X軸　false:左  true:右    //Y軸　false:上　true:下
+    protected bool directionSwitch; //方向を変えるための変数   false:上下    true:左右
+    protected float directionCount;  //方向を変えるたびにカウントが増える （コウモリ用）
+    protected float directionCount2; //方向を変えるたびにカウントが増える （ドラゴン用）
+
+    public int hp;
+    public int hp_max;
+    ParticleSystem particle;
+
+    public void LateralMovement()   //横移動
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        
+
+        if (direction == true)
+        {
+            // 座標移動
+            myTransform.Translate(speedX, 0, 0);
+
+            if (enemyX + moveX <= myTransform.position.x)
+            {
+                direction = false;
+            }
+        }
+
+        else if (direction == false)
+        {
+            myTransform.Translate(-speedX, 0, 0);
+
+            if (enemyX - moveX >= myTransform.position.x)
+            {
+                direction = true;
+            }
+        }
+    }
+
+    public void VerticalMovement()
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        if (direction == true)
+        {
+            //座標移動
+            myTransform.Translate(0, speedY, 0);
+
+            if (enemyY + moveY <= myTransform.position.y)
+            {
+                direction = false;
+            }
+        }
+
+        else if (direction == false)
+        {
+            myTransform.Translate(0, -speedY, 0);
+
+            if (enemyY - moveY >= myTransform.position.y)
+            {
+                direction = true;
+            }
+        }
+    }
+
+    public void RhombusMovement()
+    {
+
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        if(direction == true)
+        {
+            directionCount += Time.deltaTime;
+        }
+        else
+        {
+            directionCount -= Time.deltaTime;
+        }
+
+        Debug.Log((int)directionCount / 1 % 4);
+
+        switch((int)directionCount / 1 % 4)
+        {
+            case 0:
+                myTransform.Translate(speedX, -speedY, 0);
+                break;
+            case 1:
+                myTransform.Translate(-speedX, -speedY, 0);
+                break;
+            case 2:
+                myTransform.Translate(-speedX, speedY, 0);
+                break;
+            case 3:
+                myTransform.Translate(speedX, speedY, 0);
+                break;
+            
+        }
+
+
+
+    }
+
+    public void Lateral_Vertical_Movement2()
+    {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        if (directionSwitch == true && direction == true && directionCount2 != 4)  //横移動
+        {
+            // 座標移動
+            myTransform.Translate(speedX, 0, 0);
+
+            if (enemyX + moveX <= myTransform.position.x)
+            {
+                direction = false;
+                directionCount2++;
+            }
+        }
+
+        else if (directionSwitch == true && direction == false && directionCount2 != 4)
+        {
+            myTransform.Translate(-speedX, 0, 0);
+
+            if (enemyX - moveX >= myTransform.position.x)
+            {
+                direction = true;
+                directionCount2++;
+            }
+        }
+
+
+
+        if (directionSwitch == false && direction == true && directionCount2 != 4)  //縦移動
+        {
+            //座標移動
+            myTransform.Translate(0, speedY, 0);
+
+            if (enemyY + moveY <= myTransform.position.y)
+            {
+                direction = false;
+                directionCount2++;
+            }
+        }
+
+        else if (directionSwitch == false && direction == false && directionCount2 != 4)
+        {
+            myTransform.Translate(0, -speedY, 0);
+
+            if (enemyY - moveY >= myTransform.position.y)
+            {
+                direction = true;
+                directionCount2++;
+            }
+        }
+
+        if (directionCount2 == 4)
+        {
+
+
+            if (directionSwitch == true)
+            {
+                // 座標移動
+                myTransform.Translate(speedX, 0, 0);
+
+                if (enemyX <= myTransform.position.x)
+                {
+
+                    directionSwitch = false;
+                    directionCount2 = 0;
+                }
+            }
+            else
+            {
+                //座標移動
+                myTransform.Translate(0, speedY, 0);
+
+                if (enemyY <= myTransform.position.y)
+                {
+                    directionSwitch = true;
+                    directionCount2 = 0;
+                }
+
+            }
+        }
+
+    }
+
+    //死亡確認と処理
+    /*public void DestroyEnemy()
+    {
+        if (hp <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }*/
+    IEnumerator DestroyEnemy()
+    {
+        speedX = 0;
+        speedY = 0;
+        var alpha = this.GetComponent<SpriteRenderer>();
+        alpha.color = new Color(1, 1, 1, 0);
+        this.GetComponent<CircleCollider2D>().enabled = false;
+        yield return new WaitWhile(() => particle.IsAlive(true));
+        this.gameObject.SetActive(false);
+    }
+
+    //ヒットポイント減少
+    public void Damage()
+    {
+        hp--;
+    }
+
+    public void OnCollisionEnter2D(Collision2D Ball)
+    {
+        if (Ball.gameObject.tag == "Ball")
+        {
+            particle = this.GetComponent<ParticleSystem>();
+            hp--;
+            particle.Play();
+            if(hp > 0)
+            {
+                Damage();
+            }
+            else
+            {
+                StartCoroutine(DestroyEnemy());
+            }
+
+        }
+
+        if (Ball.gameObject.tag == "Wall")
+        {
+            Debug.Log("a");
+
+            directionCount += 2;
+            directionCount2++;
+
+            if (direction == true)
+            {
+                direction = false;
+            }
+            else
+            {
+                direction = true;
+            }
+        }
+
+
+    }
+
+}
