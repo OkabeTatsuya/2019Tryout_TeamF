1b7ee51f8786a13ca9bce4972a2b0b4eecbe4c1d
nkcUgai <iubu0503@gmail.com>
2019-12-19 09:51:08 +0900

    ゲームサイクルプログラム

--- 2019Tryout_TeamF_UnityProject/Assets/Ugai/EnemyPop.cs
+++ 2019Tryout_TeamF_UnityProject/Assets/Ugai/EnemyPop.cs
@@ -1,28 +1,37 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
 public class EnemyPop : SingletonMonoBehaviour<EnemyPop>
 {
     public SatgeDatabase Stage_Database;
+    public CameraMove Camera;
     public GameObject[] EnemyPoint;
-    int nowWave = 0, enemyCou = 0;
+    int nowWave = -1;
+    int maxWave = 0;
+    [HideInInspector] public int EnemyCou = 0;
 
     void Start()
     {
-        Enemy_Set();
+        maxWave = Stage_Database.Stage.Wave.Length - 1;
+        Wave_change();
     }
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Wave_change();
+        }
     }
 
 
-
-    void Enemy_Set()
+    void Wave_change()
     {
+        ++nowWave;
+        Debug.Log(Stage_Database.Stage.Wave.Length);
+
         Import_MapData Data;
         Data = JsonConvert.DeserializeObject<Import_MapData>(Stage_Database.Stage.Wave[nowWave].ToString());
+        EnemyCou = Data.MapData.Count;
         for (int i = 0; i < Data.MapData.Count; i++)
         {
             GameObject Enemy = Instantiate(Stage_Database.EnemyList[Data.MapData[i].EnemyNo]);
@@ -30,7 +39,24 @@ public class EnemyPop : SingletonMonoBehaviour<EnemyPop>
             Enemy.transform.localPosition = new Vector3(Data.MapData[i].X, -Data.MapData[i].Y, 0);
             Enemy.transform.localScale = new Vector3(Data.MapData[i].size, Data.MapData[i].size, 1);
         }
-        
+        Camera.WaveStart();
+    }
+
+    public void Enemy_Del()
+    {
+        --EnemyCou;
+        if (EnemyCou <= 0)
+        {
+            //ステージ最後
+            if (nowWave >= maxWave)
+            {
+                //リザルト
+            }
+            else
+            {
+                Wave_change();
+            }
+        }
     }
 
     public class Import_MapData
